5e96b1052c12cd4f75877905b8f475dea68f45bf
Generoso Martello <generoso@martello.com>
2015-01-15 15:02:48 +0100

    - Updated RaspberrySharp-IO with CT/CB8 auto detect and connector mappings - Added X10 RF commands decoding to XTenLib; Major library code rework (sf.net r455)

--- MigFiles/SupportLibraries/XTenLib/XTenData.cs
+++ MigFiles/SupportLibraries/XTenLib/XTenData.cs
@@ -110,7 +110,7 @@ namespace XTenLib
         PLC_ReplyToPoll = 0xC3
     }
 
-    public enum X10HouseCodes
+    public enum X10HouseCode
     {
         A = 6,
         B = 14,
@@ -130,7 +130,7 @@ namespace XTenLib
         P = 12
     }
 
-    public enum X10UnitCodes
+    public enum X10UnitCode
     {
         Unit_1 = 6,
         Unit_2 = 14,
@@ -176,10 +176,10 @@ namespace XTenLib
         /// <returns></returns>
         public static double GetPercentageValue(byte dimvalue)
         {
-            return ((double)((dimvalue) >> 3) / (double)X10_MAX_DIM);
+            return Math.Round(((double)((dimvalue) >> 3) / (double)X10_MAX_DIM), 2);
         }
 
-        public static string HouseUnitCodeFromEnum(X10HouseCodes housecode, X10UnitCodes unitcodes)
+        public static string HouseUnitCodeFromEnum(X10HouseCode housecode, X10UnitCode unitcodes)
         {
             string unit = unitcodes.ToString();
             unit = unit.Substring(unit.LastIndexOf("_") + 1);
@@ -187,117 +187,117 @@ namespace XTenLib
             return housecode.ToString() + unit;
         }
 
-        public static X10HouseCodes HouseCodeFromString(string s)
+        public static X10HouseCode HouseCodeFromString(string s)
         {
-            var houseCode = X10HouseCodes.A;
+            var houseCode = X10HouseCode.A;
             s = s.Substring(0, 1).ToUpper();
             switch (s)
             {
                 case "A":
-                    houseCode = X10HouseCodes.A;
+                    houseCode = X10HouseCode.A;
                     break;
                 case "B":
-                    houseCode = X10HouseCodes.B;
+                    houseCode = X10HouseCode.B;
                     break;
                 case "C":
-                    houseCode = X10HouseCodes.C;
+                    houseCode = X10HouseCode.C;
                     break;
                 case "D":
-                    houseCode = X10HouseCodes.D;
+                    houseCode = X10HouseCode.D;
                     break;
                 case "E":
-                    houseCode = X10HouseCodes.E;
+                    houseCode = X10HouseCode.E;
                     break;
                 case "F":
-                    houseCode = X10HouseCodes.F;
+                    houseCode = X10HouseCode.F;
                     break;
                 case "G":
-                    houseCode = X10HouseCodes.G;
+                    houseCode = X10HouseCode.G;
                     break;
                 case "H":
-                    houseCode = X10HouseCodes.H;
+                    houseCode = X10HouseCode.H;
                     break;
                 case "I":
-                    houseCode = X10HouseCodes.I;
+                    houseCode = X10HouseCode.I;
                     break;
                 case "J":
-                    houseCode = X10HouseCodes.J;
+                    houseCode = X10HouseCode.J;
                     break;
                 case "K":
-                    houseCode = X10HouseCodes.K;
+                    houseCode = X10HouseCode.K;
                     break;
                 case "L":
-                    houseCode = X10HouseCodes.L;
+                    houseCode = X10HouseCode.L;
                     break;
                 case "M":
-                    houseCode = X10HouseCodes.M;
+                    houseCode = X10HouseCode.M;
                     break;
                 case "N":
-                    houseCode = X10HouseCodes.N;
+                    houseCode = X10HouseCode.N;
                     break;
                 case "O":
-                    houseCode = X10HouseCodes.O;
+                    houseCode = X10HouseCode.O;
                     break;
                 case "P":
-                    houseCode = X10HouseCodes.P;
+                    houseCode = X10HouseCode.P;
                     break;
             }
             return houseCode;
         }
 
-        public static X10UnitCodes UnitCodeFromString(string s)
+        public static X10UnitCode UnitCodeFromString(string s)
         {
-            var unitCode = X10UnitCodes.Unit_1;
+            var unitCode = X10UnitCode.Unit_1;
             s = s.Substring(1);
             switch (s)
             {
                 case "1":
-                    unitCode = X10UnitCodes.Unit_1;
+                    unitCode = X10UnitCode.Unit_1;
                     break;
                 case "2":
-                    unitCode = X10UnitCodes.Unit_2;
+                    unitCode = X10UnitCode.Unit_2;
                     break;
                 case "3":
-                    unitCode = X10UnitCodes.Unit_3;
+                    unitCode = X10UnitCode.Unit_3;
                     break;
                 case "4":
-                    unitCode = X10UnitCodes.Unit_4;
+                    unitCode = X10UnitCode.Unit_4;
                     break;
                 case "5":
-                    unitCode = X10UnitCodes.Unit_5;
+                    unitCode = X10UnitCode.Unit_5;
                     break;
                 case "6":
-                    unitCode = X10UnitCodes.Unit_6;
+                    unitCode = X10UnitCode.Unit_6;
                     break;
                 case "7":
-                    unitCode = X10UnitCodes.Unit_7;
+                    unitCode = X10UnitCode.Unit_7;
                     break;
                 case "8":
-                    unitCode = X10UnitCodes.Unit_8;
+                    unitCode = X10UnitCode.Unit_8;
                     break;
                 case "9":
-                    unitCode = X10UnitCodes.Unit_9;
+                    unitCode = X10UnitCode.Unit_9;
                     break;
                 case "10":
-                    unitCode = X10UnitCodes.Unit_10;
+                    unitCode = X10UnitCode.Unit_10;
                     break;
                 case "11":
-                    unitCode = X10UnitCodes.Unit_11;
+                    unitCode = X10UnitCode.Unit_11;
                     break;
                 case "12":
-                    unitCode = X10UnitCodes.Unit_12;
+                    unitCode = X10UnitCode.Unit_12;
                     break;
                 case "13":
-                    unitCode = X10UnitCodes.Unit_13;
+                    unitCode = X10UnitCode.Unit_13;
                     break;
                 case "14":
-                    unitCode = X10UnitCodes.Unit_14;
+                    unitCode = X10UnitCode.Unit_14;
                     break;
                 case "15":
-                    unitCode = X10UnitCodes.Unit_15;
+                    unitCode = X10UnitCode.Unit_15;
                     break;
                 case "16":
-                    unitCode = X10UnitCodes.Unit_16;
+                    unitCode = X10UnitCode.Unit_16;
                     break;
             }
             return unitCode;
@@ -313,6 +313,33 @@ namespace XTenLib
             return ret.Trim();
         }
 
+        public static byte ReverseByte(byte originalByte)
+        {
+            int result = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                result = result << 1;
+                result += originalByte & 1;
+                originalByte = (byte)(originalByte >> 1);
+            }
+            return (byte)result;
+        }
+
+        public static void DebugLog(string prefix, string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            if (prefix.Contains(">"))
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+            }
+            else if (prefix.Contains("!"))
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+            }
+            Console.Write("[" + DateTime.Now.ToString("HH:mm:ss.ffffff") + "] ");
+            Console.WriteLine(prefix + " " + message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
     }
 
 }
