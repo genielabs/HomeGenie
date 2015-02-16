4571ddf27e19cc559332bab4e40b220212ffa699
Generoso Martello <generoso@martello.com>
2015-02-16 13:43:40 +0100

    fixed missing file error in ZWaveLib project

--- MigFiles/SupportLibraries/ZWaveLib/ZWaveEvent.cs
+++ MigFiles/SupportLibraries/ZWaveLib/ZWaveEvent.cs
@@ -24,6 +24,54 @@ using System;
 
 namespace ZWaveLib
 {
+    public enum EventParameter
+    {
+        Level,
+        ManufacturerSpecific,
+        Generic,
+        MeterKwHour,
+        MeterKvaHour,
+        MeterWatt,
+        MeterPulses,
+        MeterAcVolt,
+        MeterAcCurrent,
+        MeterPower,
+        SensorTemperature,
+        SensorHumidity,
+        SensorLuminance,
+        SensorMotion,
+        AlarmDoorWindow,
+        AlarmGeneric,
+        AlarmSmoke,
+        AlarmCarbonMonoxide,
+        AlarmCarbonDioxide,
+        AlarmHeat,
+        AlarmFlood,
+        AlarmTampered,
+        Configuration,
+        WakeUpInterval,
+        WakeUpNotify,
+        Association,
+        Battery,
+        NodeInfo,
+        MultiinstanceSwitchBinaryCount,
+        MultiinstanceSwitchBinary,
+        MultiinstanceSwitchMultilevelCount,
+        MultiinstanceSwitchMultilevel,
+        MultiinstanceSensorBinaryCount,
+        MultiinstanceSensorBinary,
+        MultiinstanceSensorMultilevelCount,
+        MultiinstanceSensorMultilevel,
+        ThermostatFanMode,
+        ThermostatFanState,
+        ThermostatHeating,
+        ThermostatMode,
+        ThermostatOperatingState,
+        ThermostatSetBack,
+        ThermostatSetPoint,
+        UserCode
+    }
+
     public class ZWaveEvent
     {
         public ZWaveNode Node;
