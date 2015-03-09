270e0558d4ac9a13dd79b4649710717de88c6e95
Alexander Sidorenko <sidorlutiy@gmail.com>
2015-03-09 19:10:43 +0300

    Added ICommandClass and CommandClassFactory.

---
+++ MigFiles/SupportLibraries/ZWaveLib/Handlers/CommandClassFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ZWaveLib.Handlers
+{
+    class CommandClassFactory
+    {
+        public static ICommandClass GetCommandClass(byte ccId)
+        {
+            if(_commandClasses == null)
+                _commandClasses = CollectCommandClasses();
+
+            Type type;
+            if (!_commandClasses.TryGetValue(ccId, out type)) 
+                return null;
+
+            var cc = (ICommandClass)Activator.CreateInstance(type);
+            return cc;
+        }
+
+        private static Dictionary<byte, Type> _commandClasses;
+
+        private static Dictionary<byte, Type> CollectCommandClasses()
+        {
+            var iCommandClassTypes = new Dictionary<byte, Type>();
+            var iType = typeof(ICommandClass);
+            var types = Assembly.GetExecutingAssembly().GetTypes().Where(t => iType.IsAssignableFrom(t));
+
+            foreach (var type in types)
+            {
+                var cc = (ICommandClass)Activator.CreateInstance(type);
+                var id = cc.GetCommandClassId();
+                _commandClasses.Add(id, type);
+            }
+
+            return iCommandClassTypes;
+        }
+    }
+}
---
+++ MigFiles/SupportLibraries/ZWaveLib/ICommandClass.cs
@@ -0,0 +1,17 @@
+﻿namespace ZWaveLib
+{
+    internal interface ICommandClass
+    {
+        /// <summary>
+        /// Returns Id of Command class
+        /// </summary>
+        /// <returns>command class Id</returns>
+        byte GetCommandClassId();
+
+        /// <summary>Processes the message and returns corresponding ZWaveEvent</summary>
+        /// <param name="node">the Node triggered the command</param>
+        /// <param name="message">command part of ZWave message (without headers and checksum)</param>
+        /// <returns></returns>
+        ZWaveEvent GetEvent(ZWaveNode node, byte[] message);
+    }
+}
