049ad01e8ad0c6805f03765d690bea6072944db0
snagytx <snagytx@gmail.com>
2015-04-07 10:55:12 -0500

    Update AES_work.cs
    cleanup

--- MigFiles/SupportLibraries/ZWaveLib/AES_work.cs
+++ MigFiles/SupportLibraries/ZWaveLib/AES_work.cs
@@ -11,43 +11,9 @@ namespace ZWaveLib.Devices
     public class AES_work
     {
 
-
-        /*internal byte[] NetworkKey = new byte[] { 
-            (byte)0x01,
-            (byte)0x02,
-            (byte)0x03,
-            (byte)0x04,
-            (byte)0x05,
-            (byte)0x06,
-            (byte)0x07,
-            (byte)0x08,
-            (byte)0x09,
-            (byte)0x0A,
-            (byte)0x0B,
-            (byte)0x0C,
-            (byte)0x0D,
-            (byte)0x0E,
-            (byte)0x0F,
-            (byte)0x10};
-        */
-
-
         internal byte[] zeroIV = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
-        //public byte[] EncryptKey = new byte[] { 0x85, 0x22, 0x71, 0x7d, 0x3a, 0xd1, 0xfb, 0xfe, 0xaf, 0xa1, 0xce, 0xaa, 0xfd, 0xf5, 0x65, 0x65 };
-//        private byte[] EncryptKey = null;
-//        private byte[] AuthKey = null;
-        /*
-        public byte[] getEncryptKey() {
-            return EncryptKey;
-        }
-
-        public byte[] getAuthKey()
-        {
-            return AuthKey;
-        }
-        */
-        public byte[] generateKey1(byte[] nc, byte[] plaintext) {
+    public byte[] generateKey1(byte[] nc, byte[] plaintext) {
             byte[] tmp = new byte[zeroIV.Length];
 
             Array.Copy(EncryptMessage(nc, zeroIV, plaintext, CipherMode.CBC), tmp, 16);
@@ -58,7 +24,6 @@ namespace ZWaveLib.Devices
         {
             byte[] processed = new byte[plaintext.Length];
             int len = (plaintext.Length % 16)*16;
-//            int len = 2048;
             byte[] l_plaintext = new byte[len];
 
             byte[] encrypted = EncryptMessage(nc, iv, l_plaintext, CipherMode.CBC);
@@ -90,7 +55,6 @@ namespace ZWaveLib.Devices
             rijndael.Key = nc;
             rijndael.IV = iv;
             rijndael.Mode = cm;
-//            rijndael.Padding = PaddingMode.Zeros;
 
             return EncryptBytes(rijndael, plaintext);
         }
