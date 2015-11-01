a79fd6e95b27baa471e0d3147f0d4af5d7e9009a
Generoso Martello <generoso@martello.com>
2015-11-01 21:59:04 +0100

    - Fixed 'continuous Event Stream disconnection' bug when running on mono ARM-hf (eg. Raspberry Pi) - Updated Spanish locales - Disabled periodic Weather Underground program notifications in the UI - Reverted XTenLib to the using of Thread instead of Task

--- BaseFiles/Linux/startup.sh
+++ BaseFiles/Linux/startup.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
+#
+# Usage: startup_debug.sh <homegenie_folder_path>
+# Example: ./startup_debug.sh /usr/local/bin/homegenie
+#
 cd "`dirname \"$0\"`"
 if [ -f /usr/bin/mono ]
 then
-	MONO=/usr/bin/mono
+    MONO="LC_ALL=en_US.UTF-8 /usr/bin/mono"
 else
-	MONO=/usr/local/bin/mono
+    MONO="LC_ALL=en_US.UTF-8 /usr/local/bin/mono"
 fi
 
 EXITCODE="1"
 while [ "$EXITCODE" = "1" ]; do
-	if [ -z "$1" ]
-	then
-		sudo $MONO HomeGenie.exe 
-	else
-		cd $1
-		sudo $MONO HomeGenie.exe >/dev/null  2>&1 
-	fi
-	EXITCODE="$?"
-	echo "Exit code: $EXITCODE"
+    if [ -z "$1" ]
+    then
+        sudo $MONO HomeGenie.exe 
+    else
+        cd $1
+        sudo $MONO HomeGenie.exe >/dev/null  2>&1 
+    fi
+    EXITCODE="$?"
+    echo "Exit code: $EXITCODE"
 done
--- BaseFiles/Linux/startup_debug.sh
+++ BaseFiles/Linux/startup_debug.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
+#
+# Usage: startup_debug.sh <homegenie_folder_path> <debugger_ip_and_port>
+# Example: ./startup_debug.sh /usr/local/bin/homegenie 10.0.1.10:10000
+#
 cd "`dirname \"$0\"`"
 if [ -f /usr/bin/mono ]
 then
-	MONO=/usr/bin/mono
+    MONO="LC_ALL=en_US.UTF-8 /usr/bin/mono"
 else
-	MONO=/usr/local/bin/mono
+    MONO="LC_ALL=en_US.UTF-8 /usr/local/bin/mono"
 fi
 
 EXITCODE="1"
 while [ "$EXITCODE" = "1" ]; do
-	if [ -z "$1" ]
-	then
-		sudo $MONO --debug --debugger-agent="address=10.0.1.10:10000,transport=dt_socket,server=y" HomeGenie.exe 
-	else
-		cd $1
-		sudo $MONO --debug --debugger-agent="address=10.0.1.10:10000,transport=dt_socket,server=y" HomeGenie.exe >/dev/null  2>&1 
-	fi
-	EXITCODE="$?"
-	echo "Exit code: $EXITCODE"
+    if [ -z "$1" ]
+    then
+        sudo $MONO --debug --debugger-agent="address=$1,transport=dt_socket,server=y" HomeGenie.exe 
+    else
+        cd $1
+        sudo $MONO --debug --debugger-agent="address=$1,transport=dt_socket,server=y" HomeGenie.exe >/dev/null  2>&1 
+    fi
+    EXITCODE="$?"
+    echo "Exit code: $EXITCODE"
 done
