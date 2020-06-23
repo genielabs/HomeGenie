bb1ed7f00b061b5d6a2748560269b543941302e6
Gene <genemars@users.noreply.github.com>
2020-06-23 19:47:50 +0200

    Update ProgramDynamicApi.cs

--- HomeGenie/Automation/ProgramDynamicApi.cs
+++ HomeGenie/Automation/ProgramDynamicApi.cs
@@ -95,13 +95,13 @@ namespace HomeGenie
                     if (command.Data == null || (command.Data is byte[] && (command.Data as byte[]).Length == 0))
                     {
                         // receives the full request as string if there is no `request.Data` payload
-                        return handler(command.OriginalRequest.Trim('/'));
+                        response = handler(command.OriginalRequest.Trim('/'));
                     }
                     else
                     {
                         // receives the original MigInterfaceCommand if `request.Data` actually holds some data
                         // TODO: this might be be the only entry point in future releases (line #98 and #87 cases will be deprecated)
-                        return handler(command);
+                        response = handler(command);
                     }
                 }
             }
