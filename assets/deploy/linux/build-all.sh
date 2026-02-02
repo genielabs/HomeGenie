71087f179b68d86005f596e1d1f370e62c8f0ba3
Gene <genemars@users.noreply.github.com>
2026-02-02 19:53:09 +0100

    build(core): migrate to .NET 10.0

--- assets/deploy/linux/build-all.sh
+++ assets/deploy/linux/build-all.sh
@@ -1,38 +1,12 @@
-# net472
+# net10.0
 
-dotnet publish --configuration Release --framework net472
-rm -rf ./src/HomeGenie/bin/Release/net472/net472
-rm -rf ./src/HomeGenie/bin/Release/net472/publish
-cp ./src/HomeGenie/release_info.xml ./src/HomeGenie/bin/Release/net472/
-sed -i "s/%runtime%/net472/" ./src/HomeGenie/bin/Release/net472/release_info.xml
-ls -la ./src/HomeGenie/bin/Release/net472/
-
-
-# net6.0
-# this is for 32bit raspbian OS that not support `GLIBC_2.34' as of 2025-03-06
-# ---> ./HomeGenie: /lib/arm-linux-gnueabihf/libc.so.6: version `GLIBC_2.34' not found (required by ./HomeGenie)
-
-runtimes=( linux-arm )
+runtimes=( linux-arm linux-arm64 linux-x64 osx-x64 win-x64 )
 for runtime in "${runtimes[@]}"
 do
 	echo "Building for '${runtime}' runtime."
-  dotnet publish --configuration Release --framework net6.0 --runtime ${runtime} --self-contained
-  rm -rf ./src/HomeGenie/bin/Release/net6.0/${runtime}/publish
-  cp ./src/HomeGenie/release_info.xml ./src/HomeGenie/bin/Release/net6.0/${runtime}/
-  sed -i "s/%runtime%/${runtime}/" ./src/HomeGenie/bin/Release/net6.0/${runtime}/release_info.xml
-  ls -la ./src/HomeGenie/bin/Release/net6.0/${runtime}
-done
-
-
-# net9.0
-
-runtimes=( linux-arm64 linux-x64 osx-x64 win-x64 )
-for runtime in "${runtimes[@]}"
-do
-	echo "Building for '${runtime}' runtime."
-  dotnet publish --configuration Release --framework net9.0 --runtime ${runtime} --self-contained
-  rm -rf ./src/HomeGenie/bin/Release/net9.0/${runtime}/publish
-  cp ./src/HomeGenie/release_info.xml ./src/HomeGenie/bin/Release/net9.0/${runtime}/
-  sed -i "s/%runtime%/${runtime}/" ./src/HomeGenie/bin/Release/net9.0/${runtime}/release_info.xml
-  ls -la ./src/HomeGenie/bin/Release/net9.0/${runtime}
+  dotnet publish --configuration Release --framework net10.0 --runtime ${runtime} --self-contained -m:1
+  rm -rf ./src/HomeGenie/bin/Release/net10.0/${runtime}/publish
+  cp ./src/HomeGenie/release_info.xml ./src/HomeGenie/bin/Release/net10.0/${runtime}/
+  sed -i "s/%runtime%/${runtime}/" ./src/HomeGenie/bin/Release/net10.0/${runtime}/release_info.xml
+  ls -la ./src/HomeGenie/bin/Release/net10.0/${runtime}
 done
--- assets/deploy/linux/pack-all.sh
+++ assets/deploy/linux/pack-all.sh
@@ -1,29 +1,10 @@
-# net472
-
 cd ./src/HomeGenie/bin/Release/
 mkdir artifacts
-mv net472 homegenie
-zip homegenie_${VERSION_NUMBER}_net472.zip -r homegenie
-mv homegenie_${VERSION_NUMBER}_net472.zip ./artifacts/
 
-# net6.0
+# net10.0
 
-runtimes=( linux-arm )
-cd net6.0
-for runtime in "${runtimes[@]}"
-do
-	echo "Creating release bundle for '${runtime}' runtime."
-  mv ${runtime} homegenie
-  zip homegenie_${VERSION_NUMBER}_${runtime}.zip -r homegenie
-  mv homegenie_${VERSION_NUMBER}_${runtime}.zip ../artifacts/
-  rm -rf homegenie
-done
-cd ..
-
-# net9.0
-
-runtimes=(linux-arm64 linux-x64 osx-x64 win-x64 )
-cd net9.0
+runtimes=( linux-arm linux-arm64 linux-x64 osx-x64 win-x64 )
+cd net10.0
 for runtime in "${runtimes[@]}"
 do
 	echo "Creating release bundle for '${runtime}' runtime."
